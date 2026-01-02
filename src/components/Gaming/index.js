79f6344bbf873a3376c29675adb9d6a836600ed4
Amalraj MP <amalrajmp72@gmail.com>
2026-01-02 07:31:29 +0000

    feat: add apiStatus state and conditional rendering for gaming page

--- src/components/Gaming/index.js
+++ src/components/Gaming/index.js
@@ -1,5 +1,8 @@
 import { Component } from 'react'
 import Cookies from 'js-cookie'
+
+import { ThreeDots } from 'react-loader-spinner'
+
 import { SiYoutubegaming } from 'react-icons/si'
 
 import Header from '../Header'
@@ -14,19 +17,31 @@ import {
   GamingIconWrapper,
   GamingTitle,
   VideosSection,
+  LoaderContainer,
   VideosList,
+  FailureContainer,
+  FailureImage,
+  FailureHeading,
+  FailureDescription,
+  RetryButton,
 } from './styledComponents'
 
+const apiStatusConstants = {
+  initial: 'INITIAL',
+  inProgress: 'IN_PROGRESS',
+  success: 'SUCCESS',
+  failure: 'FAILURE',
+}
 class Gaming extends Component {
-  state = {
-    gamingVideosList: [],
-  }
+  state = { apiStatus: apiStatusConstants.initial, gamingVideosList: [] }
 
   componentDidMount() {
     this.getGamingVideos()
   }
 
   getGamingVideos = async () => {
+    this.setState({ apiStatus: apiStatusConstants.inProgress })
+
     const jwtToken = Cookies.get('jwt_token')
 
     const response = await fetch('https://apis.ccbp.in/videos/gaming', {
@@ -37,20 +52,71 @@ class Gaming extends Component {
 
     if (response.ok) {
       const data = await response.json()
-      const updatedData = data.videos.map((each) => ({
+      const formattedData = data.videos.map((each) => ({
         id: each.id,
         title: each.title,
         thumbnailUrl: each.thumbnail_url,
         viewCount: each.view_count,
       }))
 
-      this.setState({ gamingVideosList: updatedData })
+      this.setState({
+        apiStatus: apiStatusConstants.success,
+        gamingVideosList: formattedData,
+      })
+    } else {
+      this.setState({ apiStatus: apiStatusConstants.failure })
+    }
+  }
+
+  renderLoadingView = () => (
+    <LoaderContainer data-testid="loader">
+      <ThreeDots color="#3b82f6" height={50} width={50} />
+    </LoaderContainer>
+  )
+
+  renderSuccessView = () => {
+    const { gamingVideosList } = this.state
+    return (
+      <VideosList>
+        {gamingVideosList.map((each) => (
+          <GamingVideoItem key={each.id} videoDetails={each} />
+        ))}
+      </VideosList>
+    )
+  }
+
+  renderFailureView = () => (
+    <FailureContainer>
+      <FailureImage
+        src="https://assets.ccbp.in/frontend/react-js/nxt-watch-failure-view-light-theme-img.png"
+        alt="failure view"
+      />
+      <FailureHeading>Oops! Something Went Wrong</FailureHeading>
+      <FailureDescription>
+        We are having some trouble to complete your request.
+        <br />
+        Please try again.
+      </FailureDescription>
+      <RetryButton onClick={this.getGamingVideos}>Retry</RetryButton>
+    </FailureContainer>
+  )
+
+  renderVideosList = () => {
+    const { apiStatus } = this.state
+
+    switch (apiStatus) {
+      case apiStatusConstants.inProgress:
+        return this.renderLoadingView()
+      case apiStatusConstants.success:
+        return this.renderSuccessView()
+      case apiStatusConstants.failure:
+        return this.renderFailureView()
+      default:
+        return null
     }
   }
 
   render() {
-    const { gamingVideosList } = this.state
-
     return (
       <>
         <Header />
@@ -67,13 +133,7 @@ class Gaming extends Component {
               <GamingTitle>Gaming</GamingTitle>
             </GamingHeader>
 
-            <VideosSection>
-              <VideosList>
-                {gamingVideosList.map((each) => (
-                  <GamingVideoItem key={each.id} videoDetails={each} />
-                ))}
-              </VideosList>
-            </VideosSection>
+            <VideosSection>{this.renderVideosList()}</VideosSection>
           </ContentContainer>
         </GamingPage>
       </>
--- src/components/Gaming/styledComponents.js
+++ src/components/Gaming/styledComponents.js
@@ -41,6 +41,56 @@ export const GamingTitle = styled.h1`
   font-size: 24px;
   margin: 0;
 `
+
+export const LoaderContainer = styled.div`
+  min-height: 70vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export const FailureContainer = styled.div`
+  min-height: 70vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+`
+
+export const FailureImage = styled.img`
+  width: 300px;
+  max-width: 90%;
+  margin-bottom: 20px;
+`
+
+export const FailureHeading = styled.h1`
+  font-family: 'Roboto';
+  font-size: 24px;
+  color: #1e293b;
+  margin-bottom: 8px;
+`
+
+export const FailureDescription = styled.p`
+  font-family: 'Roboto';
+  font-size: 14px;
+  color: #475569;
+  margin-bottom: 20px;
+  line-height: 1.6;
+`
+
+export const RetryButton = styled.button`
+  background-color: #4f46e5;
+  color: #ffffff;
+  font-family: 'Roboto';
+  font-size: 14px;
+  border: none;
+  border-radius: 4px;
+  padding: 8px 20px;
+  cursor: pointer;
+  outline: none;
+`
+
 export const VideosSection = styled.div`
   background-color: #f9f9f9;
   flex-grow: 1;
